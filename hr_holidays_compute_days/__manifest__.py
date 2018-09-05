ce25f5dbec47337a3766807a015dac12a6b076ed
Open Net Sàrl <info@open-net.ch>
2018-09-05 15:28:34 +0200

    Revert "[RFR] hr_holidays_compute_days: General refactoring and improvement"

--- hr_holidays_compute_days/__manifest__.py
+++ hr_holidays_compute_days/__manifest__.py
@@ -1,26 +1,22 @@
 # -*- coding: utf-8 -*-
-# Copyright 2015 iDT LABS (http://www.@idtlabs.sl)
-# Copyright 2017-2018 Tecnativa - Pedro M. Baeza
+# ©  2015 iDT LABS (http://www.@idtlabs.sl)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Employee Compute Leave Days',
-    'version': '10.0.3.0.0',
+    'version': '10.0.2.0.0',
     'category': 'Human Resources',
     'license': 'AGPL-3',
     'summary': 'Computes the actual leave days '
                'considering rest days and public holidays',
     'author': 'iDT LABS, '
-              'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/hr',
-    'depends': [
-        'hr_public_holidays',
-    ],
+    'website': 'http://idtlabs.sl',
+    'depends': ['hr', 'hr_contract', 'hr_holidays', 'hr_public_holidays'],
     'data': [
-        'views/hr_holidays_status_views.xml',
-        'views/hr_holidays_views.xml',
+        'views/hr_holidays_status.xml',
+        'security/ir.model.access.csv',
+        'security/ir_rule.xml',
     ],
     'installable': True,
-    'pre_init_hook': 'pre_init_hook',
 }
--- hr_holidays_compute_days/models/__init__.py
+++ hr_holidays_compute_days/models/__init__.py
@@ -5,4 +5,3 @@
 from . import hr_employee
 from . import hr_holidays_status
 from . import hr_holidays
-from . import resource_calendar
